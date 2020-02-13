432cfd5fe6d318a0c4dc47b6f18fcbeaa6f247ed
Nitish Jatwar <nkjatwar@gmail.com>
2020-02-13 18:06:47 +0530

    Update App.js
    first 1 commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>fdhsfghs and save to reload.
+          Edit <code>src/App.js</code>new 1 code.
         </p>
         <button className="btn btn-large btn-success">Lets do it </button>
         <a
